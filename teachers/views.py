24cb48fcd3618cae06439fda6e5cc38f8daf87e5
Nozimjon <126617482+nozimjonqahorov@users.noreply.github.com>
2026-05-20 22:49:38 +0500

    added teachergrouplist view

--- teachers/views.py
+++ teachers/views.py
@@ -1,20 +1,79 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from users.models import Roles
-from groups.models import GroupTeacher
+from groups.models import GroupTeacher, Group, GroupLesson, GroupStudent
 from lessons.forms import LessonForm
 from notifications.models import Notification,NotificationTypes
 
-class TeacherCollectingGroupListView(LoginRequiredMixin,UserPassesTestMixin,View):
-    def test_func(self):
-        return self.request.user.is_authenticated and self.request.user.role == Roles.TEACHER
 
+
+class TeacherRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        return (
+            self.request.user.is_authenticated
+            and self.request.user.role == Roles.TEACHER
+        )
+    
+
+class TeacherCollectingGroupListView(TeacherRequiredMixin, View):
     def get(self, request):
         teacher = request.user
         groups = GroupTeacher.objects.filter(teacher=teacher,group__is_opened=False).select_related('group')
 
-        return render(request,'teacher/collecting_groups.html',context={"groups":groups})
+        return render(request,'teachers/collecting_groups.html',context={"groups":groups})
+    
+
+class TeacherGroupListView(TeacherRequiredMixin, View):
+    def get(self, request):
+        teacher = request.user
+        groups = GroupTeacher.objects.filter(teacher=teacher,group__is_opened=True).select_related('group')
+
+        return render(request,'teachers/teacher_groups.html',context={"groups":groups})
+    
+
+class TeacherGroupDetailView(TeacherRequiredMixin, View):
+    def get(self, request, pk):
+
+        teacher_groups = request.user.group_teachers.all() 
+
+        for gr in teacher_groups:
+            teacher_group = GroupTeacher.objects.filter(group = gr, teacher = request.user)
+
+
+
+        group = Group.objects.prefetch_related("teachers", "students", "group_lessons").filter(pk = pk, teacher = request.user)
+        if group:
+            context = {
+                "group":group,
+                "group_teachers":group.teachers.all(),
+                "group_students":group.students.all(), 
+                "group_lessons":group.group_lessons.all()
+            }
+            return render(request, "teachers/teacher_group_detail.html", context=context)
+
+
+class TeacherLessonCreateView(TeacherRequiredMixin, View):
+    def get(self, request):
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class TeacherLessonCreateView(LoginRequiredMixin, UserPassesTestMixin, View):
     def test_func(self):
